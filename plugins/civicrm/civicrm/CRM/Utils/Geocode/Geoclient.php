267039ed4d6dd887dd5ebb2f45c5524ca687b4aa
Sophia Russell <sophia.elizabeth.russell@gmail.com>
2016-01-16 15:05:09 -0500

    working on custom geoclient script

--- plugins/civicrm/civicrm/CRM/Utils/Geocode/Geoclient.php
+++ plugins/civicrm/civicrm/CRM/Utils/Geocode/Geoclient.php
@@ -50,8 +50,19 @@ class CRM_Utils_Geocode_Geoclient {
    *
    * @var string
    */
-  static protected $_uri = '/geoclient/v1/address.json?';
 
+  static protected $_uri = '/geoclient/v1/address.xml?';
+  /**
+   * curl -v  -X GET "https://api.cityofnewyork.us/geoclient/v1/address.xml?
+   * params are houseNumber=&street=n&borough=&
+   * houseNumber
+   * street
+   * borough
+   * app_id
+   * app_key
+   *
+   * app_id=3f88824d&app_key=f2a2b535bdc9cc8f9a5e1b589002531c"
+   */
   /**
    * Function that takes an address object and gets the latitude / longitude for this
    * address. Note that at a later stage, we could make this function also clean up
@@ -65,59 +76,13 @@ class CRM_Utils_Geocode_Geoclient {
    */
   public static function format(&$values, $stateName = FALSE) {
     // we need a valid country, else we ignore
-    if (empty($values['country'])) {
-      return FALSE;
-    }
-
+    // is this necessary? can we default to US?
     $config = CRM_Core_Config::singleton();
+    $houseNumber = $values['street_number'];
+    $street = $values['street_name'];
+    $borough = CRM_Utils_Array::value('city', $values);
 
-    $add = '';
-
-    if (!empty($values['street_address'])) {
-      $add = urlencode(str_replace('', '+', $values['street_address']));
-      $add .= ',+';
-    }
-
-    $city = CRM_Utils_Array::value('city', $values);
-    if ($city) {
-      $add .= '+' . urlencode(str_replace('', '+', $city));
-      $add .= ',+';
-    }
-
-    if (!empty($values['state_province'])) {
-      if (!empty($values['state_province_id'])) {
-        $stateProvince = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_StateProvince', $values['state_province_id']);
-      }
-      else {
-        if (!$stateName) {
-          $stateProvince = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_StateProvince',
-            $values['state_province'],
-            'name',
-            'abbreviation'
-          );
-        }
-        else {
-          $stateProvince = $values['state_province'];
-        }
-      }
-
-      // dont add state twice if replicated in city (happens in NZ and other countries, CRM-2632)
-      if ($stateProvince != $city) {
-        $add .= '+' . urlencode(str_replace('', '+', $stateProvince));
-        $add .= ',+';
-      }
-    }
-
-    if (!empty($values['postal_code'])) {
-      $add .= '+' . urlencode(str_replace('', '+', $values['postal_code']));
-      $add .= ',+';
-    }
-
-    if (!empty($values['country'])) {
-      $add .= '+' . urlencode(str_replace('', '+', $values['country']));
-    }
-
-    $query = 'http://' . self::$_server . self::$_uri . $add;
+    $query = 'http://' . self::$_server . self::$_uri . 'houseNumber=?' . $houseNumber . '&street=' . $street . '&borough=' . $borough;
 
     require_once 'HTTP/Request.php';
     $request = new HTTP_Request($query);
@@ -134,16 +99,31 @@ class CRM_Utils_Geocode_Geoclient {
 
     if (isset($xml->status)) {
       if ($xml->status == 'OK' &&
-        is_a($xml->result->geometry->location,
+        is_a($xml->address,
           'SimpleXMLElement'
         )
       ) {
-        $ret = $xml->result->geometry->location->children();
-        if ($ret->lat && $ret->lng) {
-          $values['geo_code_1'] = (float) $ret->lat;
-          $values['geo_code_2'] = (float) $ret->lng;
-          return TRUE;
-        }
+        $ret = $xml->address->children();
+        $BBL = $ret->bbl;
+        echo $ret;
+        echo $xml;
+
+        require_once 'CRM/Core/BAO/CustomField.php';
+        $fieldLabel = 'BBL';
+        $groupTitle = 'BBL-address';
+        $customFieldID = CRM_Core_BAO_CustomField::getCustomFieldID( $fieldLabel, $groupTitle );
+
+        $custom_fields = array('BBL' => 'BBL_' + $customFieldID);
+
+        $objectId = $values['id'];
+        $contact_id = $objectId;
+        require_once 'CRM/Core/BAO/CustomValueTable.php';
+
+        $set_params = array('entityID' => $contact_id,
+          $custom_fields['BBL'] => $BBL);
+        CRM_Core_BAO_CustomValueTable::setValues($set_params);
+
+        return TRUE;
       }
       elseif ($xml->status == 'OVER_QUERY_LIMIT') {
         CRM_Core_Error::debug_var('Geocoding failed. Message from Geoclient: ', (string ) $xml->status);
@@ -152,10 +132,6 @@ class CRM_Utils_Geocode_Geoclient {
         return FALSE;
       }
     }
-
-    // reset the geo code values if we did not get any good values
-    $values['geo_code_1'] = $values['geo_code_2'] = 'null';
     return FALSE;
   }
-
 }
