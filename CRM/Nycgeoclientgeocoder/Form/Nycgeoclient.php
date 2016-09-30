a34619849f1825d54ba652e0d1b300a07bcbd5ab
Jon goldberg <jon@palantetech.coop>
2016-09-30 13:45:49 -0400

    Create a barebones but working settings page to set API App ID and Key

--- CRM/Nycgeoclientgeocoder/Form/Nycgeoclient.php
+++ CRM/Nycgeoclientgeocoder/Form/Nycgeoclient.php
@@ -8,55 +8,57 @@ require_once 'CRM/Core/Form.php';
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
  */
 class CRM_Nycgeoclientgeocoder_Form_Nycgeoclient extends CRM_Core_Form {
-
-  protected $_settings = array(
-    'nycapiAppId' => 'NYC Api App ID',
-    'nycapiKey' => 'NYC Api Key',
-  );
-
-  public function buildQuickForm() {
-
-    // add form elements
-    $this->add(
-      'text', // field type
-      'nycapiAppId', // field name
-      ts('NYC API App ID') // field label
-    );
-    $this->add(
-      'text', // field type
-      'nycapiKey', // field name
-      ts('NYC API Key') // field label
-    );
+  private $_settingFilter = array('group' => 'nycapi');
+  //everything from this line down is generic & can be re-used for a setting form in another extension
+  //actually - I lied - I added a specific call in getFormSettings
+  private $_submittedValues = array();
+  private $_settings = array();
+  function buildQuickForm() {
+    $settings = $this->getFormSettings();
+    foreach ($settings as $name => $setting) {
+      if (isset($setting['quick_form_type'])) {
+        $add = 'add' . $setting['quick_form_type'];
+        if ($add == 'addElement') {
+          $this->$add($setting['html_type'], $name, ts($setting['title']), CRM_Utils_Array::value('html_attributes', $setting, array ()));
+        }
+        elseif ($add == 'addtext') {
+          $this->add('text', $name, ts($setting['title']));
+        }
+        else {
+          $this->$add($name, ts($setting['title']));
+        }
+        $this->assign("{$setting['description']}_description", ts('description'));
+      }
+    }
     $this->addButtons(array(
-      array(
+      array (
         'type' => 'submit',
         'name' => ts('Submit'),
         'isDefault' => TRUE,
-      ),
+      )
     ));
-
     // export form elements
     $this->assign('elementNames', $this->getRenderableElementNames());
+CRM_Core_Error::debug_var('elementNames', $this->getRenderableElementNames());
     parent::buildQuickForm();
   }
-
-  public function postProcess() {
-    CRM_Core_Error::debug_var('this', $this);
+  function postProcess() {
+    $this->_submittedValues = $this->exportValues();
+    $this->saveSettings();
+    parent::postProcess();
   }
-
   /**
    * Get the fields/elements defined in this form.
    *
    * @return array (string)
    */
-  public function getRenderableElementNames() {
+  function getRenderableElementNames() {
     // The _elements list includes some items which should not be
-    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
-    // items don't have labels.  We'll identify renderable by filtering on
+    // auto-rendered in the loop -- such as "qfKey" and "buttons". These
+    // items don't have labels. We'll identify renderable by filtering on
     // the 'label'.
     $elementNames = array();
     foreach ($this->_elements as $element) {
-      /** @var HTML_QuickForm_Element $element */
       $label = $element->getLabel();
       if (!empty($label)) {
         $elementNames[] = $element->getName();
@@ -64,5 +66,40 @@ class CRM_Nycgeoclientgeocoder_Form_Nycgeoclient extends CRM_Core_Form {
     }
     return $elementNames;
   }
-
+  /**
+   * Get the settings we are going to allow to be set on this form.
+   *
+   * @return array
+   */
+  function getFormSettings() {
+    if (empty($this->_settings)) {
+      $settings = civicrm_api3('setting', 'getfields', array('filters' => $this->_settingFilter));
+    }
+    $settings = $settings['values'];
+    return $settings;
+  }
+  /**
+   * Get the settings we are going to allow to be set on this form.
+   *
+   * @return array
+   */
+  function saveSettings() {
+    $settings = $this->getFormSettings();
+    $values = array_intersect_key($this->_submittedValues, $settings);
+    civicrm_api3('setting', 'create', $values);
+  }
+  /**
+   * Set defaults for form.
+   *
+   * @see CRM_Core_Form::setDefaultValues()
+   */
+  function setDefaultValues() {
+    $existing = civicrm_api3('setting', 'get', array('return' => array_keys($this->getFormSettings())));
+    $defaults = array();
+    $domainID = CRM_Core_Config::domainID();
+    foreach ($existing['values'][$domainID] as $name => $value) {
+      $defaults[$name] = $value;
+    }
+    return $defaults;
+  }
 }
