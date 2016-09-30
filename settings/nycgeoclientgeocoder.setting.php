8ae122d53f6434367c3a5d9cc9d7b3da5799d57d
Jon goldberg <jon@palantetech.coop>
2016-09-30 13:05:01 -0400

    Declare settings for API Key and App ID

---
+++ settings/nycgeoclientgeocoder.setting.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ *
+ * package CRM
+ * @copyright CiviCRM LLC (c) 2004-2013
+ * $Id$
+ *
+ */
+/*
+ * Settings metadata file.
+ */
+
+return array(
+  'customDataExclude' => array(
+    'group_name' => 'NYC API',
+    'group' => 'nycapi',
+    'name' => 'nycapiAppId',
+    'type' => 'String',
+    'default' => NULL,
+    'add' => '4.6',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => 'NYC API App ID',
+    'help_text' => 'NYC API App ID',
+  ),
+  'individualTracking' => array(
+    'group_name' => 'NYC API',
+    'group' => 'nycapi',
+    'name' => 'nycapiKey',
+    'type' => 'String',
+    'default' => NULL,
+    'add' => '4.6',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => 'NYC API Key',
+    'help_text' => 'NYC API Key',
+  ),
+);
+
